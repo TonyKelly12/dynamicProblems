d57f0b813e0ed0d82160c4ee5218f003d33b78a7
TonyKelly12 <tonykelly1286@gmail.com>
2021-05-29 18:04:29 -0500

    Stairs problem

---
+++ test.js
@@ -0,0 +1,100 @@
+const numQue = [];
+let solutions = 0;
+let memo = { 2: 2, 1: 1 };
+
+function takeSteps(n) {
+  if (n > 0) {
+    const minusOne = n - 1;
+    const minusTwo = n - 2;
+    return [minusOne, minusTwo];
+  } else {
+    return [];
+  }
+}
+
+function setQue(optionsArray) {
+  if (optionsArray[0] > 0) numQue.push(optionsArray[0]);
+  if (optionsArray[1] > 0) numQue.push(optionsArray[1]);
+}
+
+function atTop(optionsArray) {
+  if (optionsArray[0] === 0) solutions++;
+  if (optionsArray[1] === 0) solutions = solutions + 2;
+}
+
+function tookOneStep(num, originalNumber) {
+  const oneStepOptions = takeSteps(num);
+  const shouldAddToMemo = [];
+  oneStepOptions.forEach((n) => {
+    const isInMemo = inMemo(n);
+    if (isInMemo) {
+        shouldAddToMemo.push(n)
+    } else {
+        if(n > 0) numQue.push(n);
+    };
+
+    // Push to que if not in memo ???
+  });
+  if (shouldAddToMemo.length === 2) addToMemo(oneStepOptions, originalNumber);
+}
+
+function tookTwoSteps(num, originalNumber) {
+  const twoStepsOptions = takeSteps(num);
+  const shouldAddToMemo = [];
+  twoStepsOptions.forEach((n) => {
+    const isInMemo = inMemo(n);
+    if (isInMemo) {
+        shouldAddToMemo.push(n)
+    } else {
+       if(n > 0) numQue.push(n);
+    };
+
+    // Push to que if not in memo ???
+  });
+  if (shouldAddToMemo.length === 2) addToMemo(twoStepsOptions, originalNumber);
+}
+
+function inMemo(number) {
+  if (memo[number]) {
+    solutions = solutions + memo[number];
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function addToMemo(stepsArray, originalNumber) {
+  const oneStepMemo = stepsArray[0];
+  const twoStepsMemo = stepsArray[0];
+  memo[originalNumber] = memo[oneStepMemo] + memo[twoStepsMemo];
+  addToSolutions(memo[originalNumber]);
+}
+
+function addToSolutions(number) {
+  solutions = solutions + number;
+}
+
+function climbStairs(n) {
+  if (n > 0) {
+    const optionsArray = takeSteps(n);
+    setQue(optionsArray);
+    atTop(optionsArray);
+    tookOneStep(optionsArray[0], n);
+    tookTwoSteps(optionsArray[1], n);
+    if (numQue.length > 0) {
+      console.log(memo);
+      let newNum = numQue.pop();
+      console.log("numQue", numQue);
+      climbStairs(newNum, memo, numQue, solutions);
+    }
+
+    return solutions;
+  }
+  return solutions;
+}
+
+// console.log(climbStairs(1))
+// console.log(climbStairs(2))
+// console.log(climbStairs(4))
+// console.log(climbStairs(5))
+console.log(climbStairs(5));
